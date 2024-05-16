19470b485b2d8b35c02d92480bf9778fd7c25f04
vinniiee <vs28245@gmail.com>
2024-05-16 18:07:16 +0530

    add skeleton component

--- src/app/topics/[topic]/posts/page.tsx
+++ src/app/topics/[topic]/posts/page.tsx
@@ -12,8 +12,11 @@ const TopicShow = ({ params }: TopicShowProps) => {
   const { topic } = params;
   return (
     <div className="flex gap-4 justify-between py-8">
+      <div className="flex flex-col gap-2">
+      <h1 className="text-4xl border-b-2 mb-4 pb-2">{topic}</h1>
       <div className="w-full">
         <PostList fetchPosts={() => fetchPostsWithTopic(topic)} />
+      </div>
       </div>
       <div className="flex flex-col justify-start items-center">
       <CreatePostForm topic={topic} />
